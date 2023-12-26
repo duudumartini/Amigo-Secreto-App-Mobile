d0fef8832367a5873e6e8e5f69ff555679ea2997
Eduardo Martini <123970080+duudumartini@users.noreply.github.com>
2023-12-26 16:32:29 -0300

    Update

---
+++ app/src/main/java/com/app/amigosecreto/fragment_revela_main.kt
@@ -0,0 +1,11 @@
+package com.app.amigosecreto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class fragment_revela_main : Fragment(R.layout.fragment_revela_main) {
+
+}
