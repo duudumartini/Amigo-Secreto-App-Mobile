211539bccbe50b7ca3dda143e90fa7bca8209cdd
Eduardo Martini <123970080+duudumartini@users.noreply.github.com>
2023-12-27 01:47:56 -0300

    update

---
+++ app/src/main/java/com/app/amigosecreto/fragment_resultado_online.kt
@@ -0,0 +1,13 @@
+package com.app.amigosecreto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class fragment_resultado_online : Fragment(R.layout.fragment_resultado_online) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
